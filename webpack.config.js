32beb32c2d74c1bbce20f7047ffbed9e50fc76c3
王杰 <jie985563349@outlook.com>
2024-04-28 15:02:53 +0800

    build: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,12 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'daigo.js',
     library: {
-      type: 'commonjs2'
+      type: 'commonjs2',
     },
     clean: true,
   },
